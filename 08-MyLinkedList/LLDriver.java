6d21e2b37904c6fe6b675da28efb4ec62ff4743e
ericwongg <wonggeric@gmail.com>
2014-03-19 10:13:31 -0400

    Added basic find, add, length, etc. functions

--- 08-MyLinkedList/LLDriver.java
+++ 08-MyLinkedList/LLDriver.java
@@ -3,14 +3,24 @@ public class LLDriver {
     public static void main(String[] args) {
 
 	MyLinkedList L = new MyLinkedList();
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Sully");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Mike");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Randall");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Boo");
+	System.out.println(L);
+       
+	System.out.println("Sully is at " + L.find("Sully"));
+	System.out.println("Length= " + L.length());
+
+	L.add(2, "Steve");
+	System.out.println(L);
+	L.set(3, "Jobs");
+	System.out.println(L);
+	System.out.println(L.remove(2));
 	System.out.println(L);
 
     }
--- 08-MyLinkedList/MyLinkedList.java
+++ 08-MyLinkedList/MyLinkedList.java
@@ -13,7 +13,85 @@ public class MyLinkedList {
     }
 
     public String toString() {
-	return "" + head;
+	String res = "";
+	Node temp = head;
+	while (temp!=null) {
+	    res = res + temp.getData() + ", ";
+	    temp = temp.getNext();
+	}
+	return res;
     }
+
+    public void add(int i, String s) {
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext(); 
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	Node add = new Node(s);
+	add.setNext(temp.getNext());
+	temp.setNext(add);
+    }
+
+    public String get(int i) {
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	return temp.getData();
+    }
+
+    public String set(int i, String s) {
+	String res;
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	res = temp.getData();
+	temp.setData(s);
+	return res;
+    }
+
+    public String remove(int i) {
+	String res;
+	Node temp = head;
+	for (int j=0;j<i-1;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-2)
+		System.exit(10);
+	}
+	res = temp.getNext().getData();
+	temp.setNext(temp.getNext().getNext());
+	return res;
+    }
+
+    public int find(String s) {
+	int i = 0;
+	Node temp = head;
+	while (temp!=null) {
+	    if (temp.getData().equals(s))
+		return i;
+	    else
+		temp = temp.getNext();
+		i++;
+	}
+	return -1;
+    }
+
+    public int length() {
+	int l = 0;
+	Node temp = head;
+	while (temp!=null) {
+	    temp = temp.getNext();
+	    l++;
+	}
+	return l;
+    }
+
 
 }
