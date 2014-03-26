49fe45f40fd44cbbb8d14c5df627f69f150cd76f
ericwongg <wonggeric@gmail.com>
2014-03-26 18:22:58 -0400

    Sorry for late post, I thought this was addone to HW08

---
+++ 09-LinkedList2/MyLinkedList.java
@@ -0,0 +1,98 @@
+public class MyLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public MyLinkedList2() {
+	head = new Node("");
+	tail = head;
+    }
+
+    public void add(String d) {
+	tail.setNext(new Node (d));
+	tail = tail.getNext();
+    }
+
+    public String toString() {
+	String res = "";
+	Node temp = head;
+	while (temp!=null) {
+	    res = res + temp.getData() + ", ";
+	    temp = temp.getNext();
+	}
+	return res;
+    }
+
+    public void add(int i, String s) {
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext(); 
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	Node add = new Node(s);
+	add.setNext(temp.getNext());
+	temp.setNext(add);
+    }
+
+    public String get(int i) {
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	return temp.getData();
+    }
+
+    public String set(int i, String s) {
+	String res;
+	Node temp = head;
+	for (int j=0;j<i;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-1)
+		System.exit(10);
+	}
+	res = temp.getData();
+	temp.setData(s);
+	return res;
+    }
+
+    public String remove(int i) {
+	String res;
+	Node temp = head;
+	for (int j=0;j<i-1;j++) {
+	    temp = temp.getNext();
+	    if (temp.equals(null) && j!=i-2)
+		System.exit(10);
+	}
+	res = temp.getNext().getData();
+	temp.setNext(temp.getNext().getNext());
+	return res;
+    }
+
+    public int find(String s) {
+	int i = 0;
+	Node temp = head;
+	while (temp!=null) {
+	    if (temp.getData().equals(s))
+		return i;
+	    else
+		temp = temp.getNext();
+		i++;
+	}
+	return -1;
+    }
+
+    public int size() {
+	int l = -1;
+	Node temp = head;
+	while (temp!=null) {
+	    temp = temp.getNext();
+	    l++;
+	}
+	return l;
+    }
+
+
+}
