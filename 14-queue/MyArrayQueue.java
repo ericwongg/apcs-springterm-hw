1b8b3da4c00f70f0188f6d7e65120615abca1900
ericwongg <wonggeric@gmail.com>
2014-04-24 10:09:23 -0400

    hw 14

---
+++ 14-queue/MyArrayQueue.java
@@ -0,0 +1,22 @@
+//dequeue - return head & add 1 to head
+//          when tail get to end, wrap around (use if or %mod)
+//empty - when head == tail
+//full - when tail == head-1
+
+public class MyArrayQueue {
+
+    private String[] stack;
+
+    public MyArrayQueue() {
+
+    }
+
+    public void enqueue(String s) {
+
+    }
+
+    public String dequeue() {
+
+    }
+
+}
